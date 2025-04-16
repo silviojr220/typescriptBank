a8a3907bff4320c16fc6f5f440f6427260fc4307
SilvioClaudioJr <silvioclaudiojr10@gmail.com>
2025-04-16 14:54:29 -0300

    commit1

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -1,10 +1,3 @@
-let saldo: number = 3000;
-
-const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
-}
-
 const elementoFormulario = document.querySelector('.block-nova-transacao form') as HTMLFormElement;
 elementoFormulario.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -17,13 +10,13 @@ elementoFormulario.addEventListener('submit', function (event) {
     const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
     const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
 
-    let tipoTransacao: string = inputTipoTransacao.value;
+    let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
     let valor: number = inputValor.valueAsNumber;
     let data: Date = new Date(inputData.value);
 
-    if (tipoTransacao == 'Depósito') {
+    if (tipoTransacao == TipoTransacao.DEPOSITO) {
         saldo += valor;
-    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+    } else if (tipoTransacao == TipoTransacao.TRANSFERENCIA || tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
         saldo -= valor;
     } else {
         alert('Tipo de Transação é invalido!');
@@ -32,7 +25,7 @@ elementoFormulario.addEventListener('submit', function (event) {
 
     elementoSaldo.textContent = saldo.toString();
 
-    const novaTransacao = {
+    const novaTransacao: Transacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
---
+++ src/saldo-component.ts
@@ -0,0 +1,6 @@
+let saldo: number = 3000;
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement;
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
